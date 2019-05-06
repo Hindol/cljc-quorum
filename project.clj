2487d0b06c6fd2e5b05529f1df75a447d365c0c7
Hindol Adhya <Hindol.Adhya@microsoft.com>
2019-05-06 12:10:39 +0530

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject monq "0.0.1"
+(defproject org.github.hindol/cljc-quorum "0.0.1"
   :description "Quroum (blockchain) JSON-RPC wrapper in Clojure"
-  :url "https://github.com/Hindol/monq"
+  :url "https://github.com/Hindol/cljc-quorum"
   :license {:name "MIT License"
             :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.10.0"]
@@ -8,5 +8,4 @@
                  [cljs-ajax "0.8.0"]
                  [cheshire "5.8.1"]]
   :plugins [[lein-cljsbuild "1.1.7"]]
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :deploy-repositories [["clojars"  {:sign-releases false}]])
